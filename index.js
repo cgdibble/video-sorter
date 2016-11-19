11fe7c9d67bd987056c3d8bab6e4bb93ae1324ae
Charlie Dibble <cgdibble@gmail.com>
2016-11-19 15:27:36 -0800

    moved notes to index and out of unsaved file

--- index.js
+++ index.js
@@ -59,3 +59,22 @@ ffprobe.stdout
 
 
 */
+
+
+
+
+
+/*
+
+ffmpeg -y -i niceViewValley.MP4 -c:v libx264 -b:v 5000k -pass 1 -f mp4 NUL && \
+ffmpeg -i niceViewValley.MP4  -c:v libx264 -b:v 5000k -maxrate 10000k -bufsize 10000k -pass 2 Test-Encode.mp4
+
+Run ffprobe, take average and standard deviation. Run ffmpeg command above but with the 110% constrained vbr (so set -maxrate to 110% of bitrate)
+
+
+bufsize should be about 1 or 2 seconds, so have it synced with bitrate
+---> set bufsize to somewhere between the same as your maxrate (one second) and twice of your maxrate (2 seconds).
+            If this is still not low enough, lower your maxrate and then re-set bufsize accordingly.
+ --> http://superuser.com/questions/945413/how-to-consider-bitrate-maxrate-and-bufsize-of-a-video-for-web
+
+*/
