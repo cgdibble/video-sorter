67e93736a20dbb8611cea6e1252e6c425368102f
Charlie Dibble <cgdibble@gmail.com>
2016-11-20 08:50:27 -0800

    component test for the calulate module that returns object with video data

--- app/calculate/index.js
+++ app/calculate/index.js
@@ -1,10 +1,16 @@
+const R = require('ramda');
 const logger = require('logger');
 const calculateAverage = require('./average');
 const extractIntegerValues = require('./extract-integer-values');
 const calculateStandardDeviation = require('./standard-deviation')(calculateAverage, extractIntegerValues, logger);
 
 module.exports = function(dataSet) {
+  const relevantValue = 'pkt_size';
+  const average = R.compose(calculateAverage, extractIntegerValues(relevantValue))(dataSet.frames);
+  const stdev = calculateStandardDeviation(relevantValue, dataSet.frames);
 
-  // const average = calculateAverage();
-  const average = calculateStandardDeviation('pkt_size', data.frames);
+  return {
+    average,
+    stdev
+  }
 }
---
+++ app/calculate/index.spec.js
@@ -0,0 +1,33 @@
+const { assert } = require('chai');
+const R = require('ramda');
+const logger = require('logger');
+
+const extractVideoData = require('./');
+const calculateAverage = require('./average');
+const extractIntegerValues = require('./extract-integer-values');
+const calculateStandardDeviation = require('./standard-deviation')(calculateAverage, extractIntegerValues, logger);
+
+describe('Calculate -> Component', () => {
+  const packetData = {
+    frames:[
+      {
+        pkt_size: '1',
+        size: '1'
+      },
+      {
+        pkt_size: '3',
+        size: '3'
+      }
+    ]
+  };
+
+  it('should return an object containing the videos average and stdev bitrate', () => {
+    const expectedData = {
+      average: 2,
+      stdev: 1
+    }
+    const videoData = extractVideoData(packetData);
+
+    assert.deepEqual(videoData, expectedData, 'Incorrect video data');
+  });
+});
